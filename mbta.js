e4f8fd78a4ed8d625e6a2673814d9997189cccca
Sulaiman Alhabib <suldev8@gmail.com>
2019-06-22 12:07:28 +0300

    Solve validation bonus

--- mbta.js
+++ mbta.js
@@ -1,14 +1,12 @@
-const claculateStops = function(start, end){
+const claculateStops = function (start, end) {
     if (start < end) {
         return end - start;
-    } else if(start > end) {
+    } else if (start > end) {
         return start - end;
     } else {
         return 0;
     }
 }
-
-
 
 const stopsBetweenStations = function (startLine, startStation, endLine, endStation) {
     const lines = {
@@ -41,10 +39,20 @@ const stopsBetweenStations = function (startLine, startStation, endLine, endStat
             'Forest Hills',
         ]
     };
+
+    if (lines[startLine] === undefined || lines[endLine] === undefined) {
+        return 'Sorry, you need to enter correct lines';
+    }
+
+    if (lines[startLine].indexOf(startStation) == -1 ||
+        lines[endLine].indexOf(endStation) == -1) {
+        return 'Sorry, you need to enter correct stations';
+    }
+
     const startPoint = lines[startLine].indexOf(startStation) + 1;
     const endPoint = lines[endLine].indexOf(endStation) + 1;
 
-    if(startLine !== endLine){
+    if (startLine !== endLine) {
         let counter = 0;
         const parkSteetStart = lines[startLine].indexOf('Park Street') + 1;
         const parkSteetEnd = lines[endLine].indexOf('Park Street') + 1;
@@ -59,6 +67,6 @@ const stopsBetweenStations = function (startLine, startStation, endLine, endStat
 }
 
 console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife')); // 0 stops
-console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station'));
-console.log(stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore'));
-console.log(stopsBetweenStations('Green', 'Government Center', 'Orange', 'North Station'));
+console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station')); // 7 stops
+console.log(stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore')); // 6 stops
+console.log(stopsBetweenStations('Green', 'Government Center', 'Orange', 'North Station')); // 3 stops
